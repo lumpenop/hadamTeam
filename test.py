4d1c13e0e9163e2ab3f18393b0c5d28377f1f9be
soonjongnim <soonjong9086@naver.com>
2020-10-21 21:07:07 +0900

    test2

--- test.py
+++ test.py
@@ -1,2 +1,19 @@
 import cv2
 from flask import Flask
+
+app = Flask(__name__)
+
+host_addr = "0.0.0.0"
+port_num = "8080"
+
+@app.route("/")
+def info():
+    return "<h1>Hello World!</h1>"
+
+@app.route("/hello")
+def hello():
+    return "<h1>JaeSung Fighting</h1>"
+
+
+if __name__ == "__main__":
+    app.run(host=host_addr, port=port_num)
